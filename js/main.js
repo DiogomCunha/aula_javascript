17b7fa1be55260c3b3f1a48886a992440fbf73d7
DiogomCunha <ddmoraes.ddm@gmail.com>
2021-08-30 15:01:14 -0300

    JS

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//var nome = "Diogo Cunha";
+//var idade = 29;
+//alert(nome + " tem " + idade + " anos ");
+
+var lista = ["maça" , "pera" , "laranja"];
+lista.push("uva");
+lista.pop();
+console.log(lista);
+
+
